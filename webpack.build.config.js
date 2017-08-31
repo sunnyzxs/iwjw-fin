c571196a3d7d6fa3bc3c1a679092ce22a431037a
zhaoxiaoshun <zhaoxiaoshun.sh@superjia.com>
2017-08-31 17:14:22 +0800

    babel

---
+++ webpack.build.config.js
@@ -0,0 +1,46 @@
+var path = require('path');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, './src/index.js'),
+    output:{
+        path:path.resolve(__dirname, './static'),
+        filename: 'index.js'
+    },
+    resolve: {
+        extensions: ['.vue', '.js'],
+        enforceExtension:false,
+    },
+    module:{
+        loaders:[
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader?presets=stage-0',
+                exclude: /node_modules/
+            },
+            {
+                test:/\.scss$/,
+                loader:'style!css!sass?sourceMap'
+            },
+            {
+                test:/\.css$/,
+                use: ['style-loader','css-loader']
+            },
+            {
+                test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
+                loader: 'url-loader',
+                query: {
+                    limit: 30,
+                    name: '[name].[ext]'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['static'])
+    ]
+}
